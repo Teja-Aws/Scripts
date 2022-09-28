a007795a290bb92c19670fe76da769ba67cf75d9
Teja <tejathotadevops9@gmail.com>
2022-09-28 09:23:28 +0530

    script

---
+++ tomcat-userdata.sh
@@ -0,0 +1,8 @@
+#/bin/bash
+sudo yum update -y
+sudo amazon-linux-extras install java-openjdk11
+wget -O /opt/apache-tomcat-9.0.67-windows-x64.zip https://dlcdn.apache.org/tomcat/tomcat-9/v9.0.67/bin/apache-tomcat-9.0.67-windows-x64.zip
+cd /opt
+unzip apache-tomcat-9.0.67-windows-x64.zip
+mv apache-tomcat-9.0.67 tomcat9
+rm -rf apache-tomcat-9.0.67-windows-x64.zip
